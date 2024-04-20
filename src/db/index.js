4faa9786832b02dfb74a2eb5861b3dfaadae916c
Sankhlavinay <vinaysankhla4233@gmail.com>
2024-04-20 18:24:55 +0530

    Utils and setup

---
+++ src/db/index.js
@@ -0,0 +1,24 @@
+import mongoose from "mongoose";
+import { DB_NAME } from "../constants.js";
+
+const connectDB = async () => {
+  try {
+    const connectionInstance = await mongoose.connect(
+      `${process.env.MONGODB_URI}/${DB_NAME}`
+    );
+
+    app.on("error", (error) => {
+      console.log("ERROR: ", error);
+      throw error;
+    });
+
+    console.log(
+      `/n MongoDB Connected !! DB Host: ${connectionInstance.connection.host}`
+    );
+  } catch (error) {
+    console.log("Connection Error: ", error);
+    process.exit(1);
+  }
+};
+
+export default connectDB;
